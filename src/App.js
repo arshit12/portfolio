35233abb6d0606fd09d1651bb32e364f0f6dc47c
Arshit <126374707+arshit12@users.noreply.github.com>
2024-06-01 18:05:39 +0530

    hh

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; // Add the missing import statement for React
+
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from './component/Header';
+import LandingSection from './component/LandingSection';
+import ProjectsSection from './component/ProjectSection';
+import ContactMeSection from './component/ContactMeSection' ;
+import Footer from './component/Footer';
+import { AlertProvider } from './context/alertConteaxt';
+import Alert from './component/Alert';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <AlertProvider>
+        <main>
+          <Header />
+          <LandingSection />
+          <ProjectsSection />
+          <ContactMeSection />
+          <Footer />
+          <Alert />
+        </main>
+      </AlertProvider>
+    </ChakraProvider>
   );
 }
 
